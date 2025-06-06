4fd4e3550fcf16daa42ebeb7a607c8a60e31562b
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-06 22:24:23 +0700

    feat: add button component and fix color theme values

---
+++ src/components/Button/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <component :is="tag" v-bind="$attrs"
+        :class="['inline-flex justify-center px-4 py-2 rounded cursor-pointer', variantClass]">
+        <slot />
+    </component>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+    tag?: 'button' | 'a' | 'RouterLink',
+    variant?: 'primary' | 'secondary'
+}>(), {
+    tag: 'button',
+    variant: 'primary'
+})
+
+const variantClass = props.variant === 'primary' ? 'bg-primary text-white hover:bg-primary/90' : 'border border-primary text-primary bg-transparent hover:bg-primary/10';
+</script>

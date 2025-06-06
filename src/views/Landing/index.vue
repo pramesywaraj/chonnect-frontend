1f8d1b6a820745a03aad898a375b460024f076e9
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-06 22:26:07 +0700

    feat: slice landing page

--- src/views/Landing/index.vue
+++ src/views/Landing/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <h1>Hello Chonnect</h1>
+  <div class="bg-white p-4 flex-auto flex flex-col">
+    <h2 class="text-primary">Chonnect.</h2>
+
+    <div class="flex-auto flex flex-col justify-center">
+      <h1 class="text-primary">Stay hi 👋 Stay close 👊</h1>
+      <p class="text-l">Where chatting feels like connecting instantly.</p>
+    </div>
+
+    <div class="flex flex-col justify-center gap-2">
+      <Button variant="primary" tag="button">Login</Button>
+      <Button variant="secondary">Register</Button>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-console.log('Hello world');
+import Button from '@components/Button/index.vue';
 </script>

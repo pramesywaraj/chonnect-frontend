32b2be8347947a48b8fc340fcadad559e4fb3088
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-16 04:42:18 +0700

    feat: register form revamp

--- src/views/Register/components/RegisterForm/index.vue
+++ src/views/Register/components/RegisterForm/index.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
 import { TextInput, PasswordInput } from '@components/Inputs';
 import { PhLock, PhAt, PhUser } from '@phosphor-icons/vue';
-import { Form } from 'vee-validate';
+import { useForm } from 'vee-validate';
 
 import { registerSchema } from '@/schemas/register-schema';
 import Button from '@components/Button/index.vue';
 
-const onSubmit = values => {
-  console.log('Form values here', values);
-};
+interface RegisterFormValues {
+  email: string;
+  name: string;
+  password: string;
+}
+
+const { handleSubmit } = useForm({
+  validationSchema: registerSchema
+});
+
+const onSubmit = handleSubmit((values: any) => {
+  const formValues = values as RegisterFormValues;
+});
 </script>
 
 <template>
-  <Form class="flex flex-col gap-2 py-4" :validation-schema="registerSchema" @submit="onSubmit">
+  <form class="flex flex-col gap-2 py-4" @submit="onSubmit">
     <TextInput
       name="email"
       type="email"
       placeholder="your email"
+      dengerMessageClass="text-white"
       :icon="{
         component: PhAt
       }"
@@ -25,6 +36,7 @@ const onSubmit = values => {
       name="name"
       type="text"
       placeholder="your name"
+      dengerMessageClass="text-white"
       :icon="{
         component: PhUser
       }"
@@ -32,11 +44,19 @@ const onSubmit = values => {
     <PasswordInput
       name="password"
       placeholder="your password"
+      dengerMessageClass="text-white"
       :icon="{
         component: PhLock
       }"
     />
-
+    <PasswordInput
+      name="confirmPassword"
+      placeholder="confirm your password"
+      dengerMessageClass="text-white"
+      :icon="{
+        component: PhLock
+      }"
+    />
     <Button tag="button" variant="secondary" type="submit">Let's Get Chatting!</Button>
-  </Form>
+  </form>
 </template>

9669d4f7cc2799a7c43dfad58012fda8114cb554
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-16 04:41:18 +0700

    feat: custom button revamp

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -1,27 +1,43 @@
+<script lang="ts" setup>
+import { computed, useAttrs } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    tag?: 'button' | 'a' | 'RouterLink';
+    variant?: 'primary' | 'secondary';
+    type?: 'submit' | 'button' | 'reset';
+  }>(),
+  {
+    tag: 'button',
+    variant: 'primary',
+    type: 'button'
+  }
+);
+const attrs = useAttrs();
+
+const mergedAttrs = computed(() => {
+  if (props.tag === 'button') {
+    return {
+      ...attrs,
+      type: props.type
+    };
+  }
+
+  return attrs;
+});
+
+const variantClass =
+  props.variant === 'primary'
+    ? 'bg-primary text-white hover:bg-primary/90'
+    : 'border border-primary text-primary bg-white hover:bg-white/50';
+</script>
+
 <template>
   <component
     :is="tag"
-    v-bind="$attrs"
+    v-bind="mergedAttrs"
     :class="['inline-flex justify-center px-4 py-2 rounded cursor-pointer', variantClass]"
   >
     <slot />
   </component>
 </template>
-
-<script lang="ts" setup>
-const props = withDefaults(
-  defineProps<{
-    tag?: 'button' | 'a' | 'RouterLink';
-    variant?: 'primary' | 'secondary';
-  }>(),
-  {
-    tag: 'button',
-    variant: 'primary'
-  }
-);
-
-const variantClass =
-  props.variant === 'primary'
-    ? 'bg-primary text-white hover:bg-primary/90'
-    : 'border border-primary text-primary bg-transparent hover:bg-primary/10';
-</script>

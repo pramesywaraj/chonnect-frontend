4123f619f65e8ad8576a262cfe30b34da6eb4173
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-23 00:21:08 +0700

    feat: add chat card

---
+++ src/views/Home/components/ChatCard/index.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import { ProfileImage } from '@components/Images';
+import { computed } from 'vue';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import isToday from 'dayjs/plugin/isToday';
+import isYesterday from 'dayjs/plugin/isYesterday';
+
+import { MessageStatusEnum } from '@/types/message';
+import { Room } from '@/types/room';
+
+interface Props {
+  room: Room;
+}
+
+dayjs.extend(isToday);
+dayjs.extend(isYesterday);
+dayjs.extend(relativeTime);
+
+const props = defineProps<Props>();
+
+const roomName = computed(() => props.room.name);
+const roomCreatedAt = computed(() => props.room.created_at);
+const latestMessage = computed(() => props.room.last_message);
+
+const isReaded = computed(() => latestMessage.value?.statuses.some(status => status.status === MessageStatusEnum.READ));
+const isNoMessage = computed(() => !latestMessage.value);
+const isMessageReaded = computed(() => (isReaded.value ? 'text-text-secondary' : 'text-text-primary'));
+const latestMessageContent = computed(() => latestMessage.value?.content);
+const latestMessageTime = computed(() => {
+  const messageDate = dayjs(latestMessage.value?.created_at ?? roomCreatedAt.value);
+
+  if (messageDate.isToday()) {
+    return messageDate.format('HH:mm');
+  } else if (messageDate.isYesterday()) {
+    return 'Yesterday';
+  } else {
+    return messageDate.format('DD/MM/YYYY');
+  }
+});
+</script>
+
+<template>
+  <div class="flex gap-3 cursor-pointer">
+    <ProfileImage :name="roomName" :image-url="latestMessage?.sender?.profile_image" />
+    <div class="flex flex-auto flex-col justify-between">
+      <p class="font-semibold text-primary">{{ roomName }}</p>
+      <p v-if="isNoMessage" class="text-sm text-text-secondary italic">chat to start the conversation</p>
+      <p v-else :class="['text-sm', isMessageReaded]">{{ latestMessageContent }}</p>
+    </div>
+    <div class="flex flex-col items-start">
+      <p class="text-xs text-text-secondary">{{ latestMessageTime }}</p>
+    </div>
+  </div>
+</template>

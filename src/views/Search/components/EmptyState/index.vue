dfc6fd102ce0c5055b5d6c1e1af42377a7f814d0
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-24 15:42:31 +0700

    feat: add empty state

---
+++ src/views/Search/components/EmptyState/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { PhMagnifyingGlass } from '@phosphor-icons/vue';
+</script>
+
+<template>
+  <div class="flex flex-col flex-auto items-center justify-center gap-4">
+    <PhMagnifyingGlass size="128" class="text-primary" />
+    <div class="flex flex-col items-center justify-center gap-2">
+      <p class="text-primary text-lg font-medium">No User Found</p>
+      <p class="text-secondary text-sm text-center">
+        We couldn't find any users matching your search. Please try again with different keywords.
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -8,6 +8,7 @@ import TextInput from '@/components/Inputs/TextInput/index.vue';
 import { User } from '@/types/user';
 
 import UserList from './components/UserList/index.vue';
+import EmptyState from './components/EmptyState/index.vue';
 
 import { mockUsers } from './__mock__';
 
@@ -22,9 +23,9 @@ const onClickBackButton = () => {
 </script>
 
 <template>
-  <div class="flex flex-col flex-auto bg-background">
+  <div class="flex flex-col flex-auto bg-background min-h-screen">
     <Navbar title="Chat with Others" :is-enable-back-button="true" :on-click-back-button="onClickBackButton" />
-    <div class="flex flex-col p-4">
+    <div class="flex flex-col p-4 flex-1">
       <TextInput
         v-model="search"
         wrapper-class="ring-primary border-2 border-primary"
@@ -32,7 +33,8 @@ const onClickBackButton = () => {
         :icon="{ component: PhMagnifyingGlass, size: 24, class: 'text-primary' }"
       />
 
-      <UserList :users="users" />
+      <UserList v-if="!search" :users="users" />
+      <EmptyState v-else />
     </div>
   </div>
 </template>

d8c8d6c36b6e7aa92c90486229d4889e432fffe1
pramesywaraj <jembarpanalar21@gmail.com>
2025-06-24 19:38:57 +0700

    feat: change Room participant type

--- src/types/room.ts
+++ src/types/room.ts
@@ -1,11 +1,21 @@
 import { Message } from './message';
 import { User } from './user';
 
+export enum ParticipantRole {
+  ADMIN = 'ADMIN',
+  MEMBER = 'MEMBER'
+}
+
+export interface Participant extends Pick<User, 'id' | 'name' | 'profile_image'> {
+  role: ParticipantRole;
+  joined_at: string;
+}
+
 export interface Room {
   id: string;
   name: string;
   is_group: boolean;
   created_at: string;
-  participants: User[];
+  participants: Participant[];
   last_message: Message | null;
 }

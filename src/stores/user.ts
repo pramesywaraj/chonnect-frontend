224803da26e7db5a853ca19f4a0141bd238d2b7e
pramesywaraj <jembarpanalar21@gmail.com>
2025-07-17 15:22:15 +0700

    feat: create user store -- its state, action, and persist option

---
+++ src/stores/user.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import type { User } from '@/types/user';
+
+interface AuthState {
+  user: User | null;
+  access_token: string | null;
+  refresh_token: string | null;
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): AuthState => ({
+    user: null,
+    access_token: null,
+    refresh_token: null
+  }),
+  actions: {
+    setUser(user: User) {
+      this.user = user;
+    },
+    setAccessToken(token: string) {
+      this.access_token = token;
+    },
+    setRefreshToken(refreshToken: string) {
+      this.refresh_token = refreshToken;
+    },
+    clearAuth() {
+      this.user = null;
+      this.access_token = null;
+      this.refresh_token = null;
+    }
+  },
+  persist: true
+});

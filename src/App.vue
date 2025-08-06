3ac99c42333ecdbb33c428eaaa17bec80d0ce18a
pramesywaraj <jembarpanalar21@gmail.com>
2025-08-06 16:52:23 +0700

    feat: add vue query developer tools

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+
+import Notification from '@/components/Notification/index.vue';
+</script>
+
 <template>
   <div class="bg-gray-100">
     <div class="mx-auto max-w-md min-h-screen flex flex-col w-full h-full">
@@ -6,10 +13,5 @@
   </div>
 
   <Notification />
+  <VueQueryDevtools position="top" button-position="relative" />
 </template>
-
-<script lang="ts" setup>
-import { RouterView } from 'vue-router';
-
-import Notification from '@/components/Notification/index.vue';
-</script>
